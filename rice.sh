95ed7d4a446ed6456c512c4184c533cf41b1dd6c
lotation <34284739+lotation@users.noreply.github.com>
2020-09-22 11:29:24 +0100

    Add files via upload

--- rice.sh
+++ rice.sh
@@ -17,7 +17,7 @@ done
 
 install() {
         echo -e "\nInstalling ${1}:"
-        pacman -S ${1} --noconfirm 1>/dev/null 2>errors.log
+        pacman -S ${1} --noconfirm 1>/dev/null 2>>errors.log
         echo -e "Done."
 }
 
@@ -25,12 +25,12 @@ aur_install() {
 	echo -e "\nInstalling ${1}:"
 	dir=./tempbuild
 	if [[ ! -e $dir ]]; then
-    		mkdir $dir
+    		sudo -u lotation mkdir $dir
 	fi
 	cd $dir
-	git clone https://aur.archlinux.org/${1}.git 1>/dev/null 2>>errors.log
+	sudo -u lotation git clone https://aur.archlinux.org/${1}.git 1>/dev/null 2>>errors.log
 	cd ${1}
-	makepkg -sirc --noconfirm --noprogressbar 1>/dev/null 2>>errors.log
+	sudo -u lotation makepkg -sirc --noconfirm --noprogressbar 1>/dev/null 2>>errors.log
 	cd ..
 	rm -rf ${1}
 	echo -e "Done."
@@ -81,11 +81,11 @@ do
 done
 
 # Installing oh-my-zsh!
-curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh 1>/dev/null 2>errors.log
+sudo -u lotation curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh 1>/dev/null 2>>errors.log
 
 # Installing spicetify Dribbblish theme
 cd "$(dirname "$(spicetify -c)")/Themes/Dribbblish"
-cp dribbblish.js ../../Extensions
+sudo -u lotation cp dribbblish.js ../../Extensions
 spicetify config extensions dribbblish.js
 spicetify config current_theme Dribbblish color_scheme base
 spicetify config inject_css 1 replace_colors 1 overwrite_assets 1
@@ -93,7 +93,7 @@ spicetify config color_scheme purple
 spicetify apply
 
 # powerlevel10k zsh theme installation
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/lotation/powerlevel10k 1>/dev/null 2>errors.log
+sudo -u lotation git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/lotation/powerlevel10k 1>/dev/null 2>>errors.log
 echo 'source /home/lotation/powerlevel10k/powerlevel10k.zsh-theme' >>! /home/lotation/.zshrc
 # font install:
 wget -P /home/lotation/.local/share/fonts -q https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
@@ -108,11 +108,11 @@ systemctl start slim.service
 
 
 # Getting hands dirty
-git clone --depth=1 https://github.com/VaughnValle/elektropunk.git 1>/dev/null 2>errors.log
+sudo -u lotation git clone --depth=1 https://github.com/VaughnValle/elektropunk.git 1>/dev/null 2>>errors.log
 cd elektropunk
-cp -r ./* /home/lotation/.config/
-mv /home/lotation/.config/.Xresources /home/lotation/
-mv /home/lotation/.config/.vimrc /home/lotation/
+sudo -u lotation cp -r ./* /home/lotation/.config/
+sudo -u lotation mv /home/lotation/.config/.Xresources /home/lotation/
+sudo -u lotation mv /home/lotation/.config/.vimrc /home/lotation/
 
 
 
