48d505b54f73dfe47549b6ff57e6df654a6ed05a
lotation <xlapsiu@gmail.com>
2021-04-11 22:57:19 +0200

    update

--- motd.sh
+++ motd.sh
@@ -13,6 +13,7 @@ KERNEL=$(uname -r)
 PKGS=$(( $(pacman -Qq | wc -l) + $(pacman -Qqm | wc -l) ))
 UPDATES=$( (checkupdates 2>/dev/null) | wc -l)
 
+# Prints the first greeting message
 greeting() {
 	clear
 	printf "%s%s%s\n" "[ $DATE $SEPARATOR $HOUR ]"
@@ -22,8 +23,9 @@ greeting() {
 	printf "\n"
 }
 
+# Calculate the usage of the given filesystem name, i.e.
+# data -> /dev/sdb4 -> 34%
 usageof() {
-    
     FSUSE=$(( $( df -h "$(fsmatch "$1")" | tail +2 | cut -b 35,36 ) / 2))
     
     for i in {1..50} ; do
@@ -36,6 +38,7 @@ usageof() {
 
 }
 
+# Grep some info about the current system (Distro, Kernel version, Number of packages and upgradable ones, etc...)
 sysinfo() {
 	printf "%s\n" "${COLOR}Distro${RESET}:       $DISTRO"
 	printf "%s\n" "${COLOR}Kernel${RESET}:       $KERNEL"
@@ -44,31 +47,34 @@ sysinfo() {
 	printf "\n"
     printf "%s\n" "${COLOR}FS Usage${RESET}:     /             [$(usageof root)]"
 	printf "%s\n" "              /home         [$(usageof home)]"
-	printf "%s\n" "              /media/DATA   [$(usageof data)]"
+	printf "%s\n" "              MacOS         [$(usageof macos)]"
 	printf "\n"
 
 }
 
+# Final message with a friendly reminder and a random quote
 goodbye() {
     printf "\n\n%s\n" "Remember the bible: ${COLOR}https://wiki.archlinux.org/${RESET}"
     printf "\n%s\n"   "\"$(fortune -n 72 -sae)\""
     printf "\n"
 }
 
+# Match the device with the partition label
 fsmatch() {
     case $1 in
         root)
             echo "/dev/sda2"
             ;;
         home)
-            echo "/dev/sdb2"
+            echo "/dev/sdb4"
             ;;
-        data)
-            echo "/dev/sdb3"
+        macos)
+            echo "/dev/sdb2"
             ;;
     esac
 }
 
+# Call all the functions, a sort of C main 
 greeting
 sysinfo
 goodbye
